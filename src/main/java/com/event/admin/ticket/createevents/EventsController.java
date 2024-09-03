9c5a46f96cc9f818e2dac1d0f480782d91f77c8c
codeartify <github@codeartify.com>
2024-09-03 15:32:15 +0200

    add handlers

--- src/main/java/com/event/admin/ticket/createevents/EventsController.java
+++ src/main/java/com/event/admin/ticket/createevents/EventsController.java
@@ -2,53 +2,30 @@ package com.event.admin.ticket.createevents;
 
 import com.event.admin.ticket.domain.Event;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.PreparedStatement;
-import java.util.Objects;
-
 @Slf4j
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api")
- public class EventsController {
+public class EventsController {
 
-    private final JdbcTemplate jdbcTemplate;
-
-    public EventsController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    private EventsHandler eventsHandler;
 
 
     // Endpoint to create a new event
     @PostMapping("/events")
     @Transactional
     public ResponseEntity<Event> createEvent(@Valid @RequestBody Event event) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement("INSERT INTO event (name, tickets_per_booker) VALUES (?, ?)", new String[]{"id"});
-            ps.setString(1, event.getName());
-            ps.setInt(2, event.getNumberOfTicketsPerBooker());
-            return ps;
-        }, keyHolder);
-
-        long newEventId = Objects.requireNonNull(keyHolder.getKey()).longValue();
-
-        Event createdEvent = jdbcTemplate.queryForObject(
-                "SELECT id, name, tickets_per_booker FROM event WHERE id = ?",
-                new Object[]{newEventId},
-                (rs, rowNum) -> new Event(rs.getLong("id"), rs.getString("name"), rs.getInt("tickets_per_booker")
-                )
-        );
-
+        var createdEvent = eventsHandler.handle(event);
         return ResponseEntity.ok(createdEvent);
     }
+
 }
--- src/main/java/com/event/admin/ticket/createtickets/TicketController.java
+++ src/main/java/com/event/admin/ticket/createtickets/TicketController.java
@@ -1,10 +1,10 @@
 package com.event.admin.ticket.createtickets;
 
-import com.event.admin.ticket.domain.*;
+import com.event.admin.ticket.domain.Ticket;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,34 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Slf4j
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api")
- public class TicketController {
+public class TicketController {
 
-    private final JdbcTemplate jdbcTemplate;
-
-    public TicketController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    private TicketHandler ticketHandler;
 
     @PostMapping("/tickets")
     @Transactional
     public ResponseEntity<List<Ticket>> createTickets(@Valid @RequestBody List<Ticket> tickets) {
-        log.info("Creating tickets...");
-        log.info("Number of tickets: {}", tickets.size());
-        log.info("Tickets: {}", tickets);
-        if (tickets.size() > 10) {
-            throw new IllegalArgumentException("Cannot purchase more than 10 tickets at a time.");
-        }
-
-        String query = "INSERT INTO ticket (price, type, qr_code, booker_id, event_id) VALUES(?, ?, ?, ?, ?)";
-        for (Ticket ticket : tickets) {
-            log.info("Ticket {}", ticket);
-            jdbcTemplate.update(query, ticket.getPrice(), ticket.getType(), ticket.getQrCode(), ticket.getBookerId(), ticket.getEvent().getId());
-        }
-        log.info("Tickets created: {}", tickets.size());
-        tickets.forEach(ticket -> log.info("Ticket type: {}, price: {}", ticket.getType(), ticket.getPrice()));
-        return ResponseEntity.ok(tickets);
+        var updatedTickets = ticketHandler.handle(tickets);
+        return ResponseEntity.ok(updatedTickets);
     }
 
 
